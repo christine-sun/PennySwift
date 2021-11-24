2f3c3ba269c1a5cc67cc7bfcffcf14065c4ed52d
Christine Sun <christinesunnjm@gmail.com>
2021-11-24 14:54:26 -0500

    simple penny

--- PennyViewController.swift
+++ PennyViewController.swift
@@ -9,30 +9,41 @@ import UIKit
 
 class PennyViewController: UIViewController {
 
+    // The text field users edit to search for an author
+    @IBOutlet weak var authorTextField: UITextField!
+    
+    // The label that displays the results of the search
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        let url = URL(string: "https://pennyandroidserver.herokuapp.com/searchresults?author=ker")!
-
-        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-            guard let data = data else { return }
-            print(data)
-            print(String(data: data, encoding: .utf8)!)
-        }
-
-        task.resume()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // This function is called every time the text inside authorTextField changes
+    @IBAction func textDidChange(_ sender: Any) {
+        
+        // Get the text in authorTextField
+        var author:String = self.authorTextField.text!
+        
+        // URL encode the author text input
+        author = author.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        // Instantiate an object of the standard class URL that will be used to send to the server an HTTP request for the specified author
+        let url = URL(string:"https://pennyandroidserver.herokuapp.com/searchresults?author=\(author)")!
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            if let data = data {
+                // data is NOT nil
+                DispatchQueue.main.async {
+                    self.resultsLabel.text = String(data: data, encoding: .utf8)!
+                }
+            } else {
+                // data IS nil
+                DispatchQueue.main.async {
+                    self.resultsLabel.text = error!.localizedDescription
+                }
+            }
+        }
+        task.resume()
     }
-    */
 
 }
---
+++ Readme.md
@@ -0,0 +1,48 @@
+#PennySwift
+
+The simplest version of Penny built on Swift.
+Models the Penny application shown in COS333 lecture.
+
+## Demo
+![](https://i.imgur.com/uRzRKlo.gif)
+
+## Important Files
+### PennyViewController
+All of the logic on the main screen lives here.
+
+**Properties**
+There are "outlets" to Main, which essentially
+connect UI components like text fields and labels
+to code.
+The outlets include:
+- authorTextField: the text field users edit to search 
+  for an author
+- resultsLabel: the label that displays the results of
+  the search
+
+**viewDidLoad()** is the first function that is called when
+the view controller (the "screen") is loaded (like "main").
+Not much code is here for Penny, but typically, code for
+styling and other setup goes here.
+
+**textDidChange** is a function that is connected with the
+"Editing Changed" action on authorTextField. This means that
+every time we edit the texxt in authorTextField, the function
+is called. Here we
+- get the text inside of authorTextField and URL-encode it
+- make a HTTP request for the specified author
+- set the text in resultsLabel to the results of the HTTP request
+
+### Main
+The user interface is built here. Can be viewed as Storyboard
+or as a XML file. Note that much of the code is auto-generated
+in the XML file.
+Key parts of XML file are under subviews
+- "PennySwift" label
+- "Enter an author" textField
+    - there is a connection with the action being textDidChange
+- "The results go here" label
+
+## Notes
+The server is identical to the PennyAndroidServer.
+I used pennyandroidserver.herokuapp.com
